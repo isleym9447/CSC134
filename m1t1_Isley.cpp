6a88fbc54e0234206e6e63df31c94f512c342ea4
Mattea <150169210+isleym9447@users.noreply.github.com>
2025-08-25 14:59:47 +0000

    initial program creation

---
+++ m1t1_Isley.cpp
@@ -0,0 +1,47 @@
+/*
+
+
+CSC 134
+Mattea Isley
+8/25/2025
+
+M1T1 -hello, user
+INPUT: run the program
+PROCESSING: none yet
+OUTPUT: return 0 (no error)
+SPECIAL FX: Greet the user
+
+
+*/
+
+
+
+
+//beginning with magic words
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //sfx: greet user
+    cout << "Hello, user!" << endl;
+
+    //output
+    return 0; //no errors
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+

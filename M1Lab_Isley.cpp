503e2ce0983f3dfc7a5292653eca8921b608d82a
Mattea <150169210+isleym9447@users.noreply.github.com>
2025-08-27 14:51:16 +0000

    added flair and ready for gathering inout

--- M1Lab_Isley.cpp
+++ M1Lab_Isley.cpp
@@ -27,17 +27,38 @@ using namespace std;
 int main()
 {
 
-    string item_name = "sword"; //replace with your item
-    int num_items = 10;
-    double cost_per = 0.25;
+    string item1_name = "sword"; //replace with your item
+    string item2_name = "staff";
+    string item3_name = "mace";
+    string item4_name = "bow and arrow";
+
+    int num_item1 = 10;
+    double cost_per = 250.75;
     
 
 
+    // give our sales pitch
+    cout << "Greetings traveler!" << endl;
+    cout << "Welcome to the Owlbear's Talon!" << endl;
+
+    cout << "----------" << endl;
+
+    cout << "We sell " << item1_name << "s " << "and " << item2_name << "s" << "!" << endl;
+    cout << "We are only selling " << item1_name << "s" << " today!" << endl;
+
+    cout << "----------" << endl;
+
+    cout << "We have " << num_item1 << " total." << endl;
+    cout << "Each " << item1_name << " is $" << cost_per << " coins" << endl;
+
+    cout << "----------" << endl;
+    cout << "----------" << endl;
+
+    cout << "How many " << item1_name << "s do you want to purchase?" << endl;
+
+    // do the processing
+    double total_cost = num_item1 * cost_per;
     
-    cout << "Hello user!" << endl;
-    cout << "Welcome to the " << item_name << " store!" << endl;
-
-     cout << "How many " << item_name << "s do you want to purchase?" << endl;
 
 
 
@@ -47,7 +68,7 @@ int main()
 
 
 
-    //output
+    //end program
     return 0; //no errors
 
 

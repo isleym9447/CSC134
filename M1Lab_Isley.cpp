99c4492611edc99b46c18f6bc8a1469de952c602
Mattea <150169210+isleym9447@users.noreply.github.com>
2025-08-27 15:09:06 +0000

    working and ready to submit

--- M1Lab_Isley.cpp
+++ M1Lab_Isley.cpp
@@ -29,10 +29,10 @@ int main()
 
 
 //establish and assign variables
-    string item1_name = "sword"; //replace with your item
-    string item2_name = "staff";
-    string item3_name = "mace";
-    string item4_name = "bow and arrow";
+    string item1_name = "Sword"; //replace with your item
+    string item2_name = "Staff";
+    string item3_name = "Mace";
+    string item4_name = "Bow and Arrow";
 
     int num_item1 = 10;
     double cost_per = 250.75;
@@ -45,12 +45,17 @@ int main()
 
     cout << "----------" << endl;
 
-    cout << "We sell " << item1_name << "s " << "and " << item2_name << "s" << "!" << endl;
+    cout << "Current inventory:" << endl; 
+    cout << item1_name << "s " << endl; 
+    cout << item2_name << "s " << endl; 
+    cout << item3_name << "s " << endl; 
+    cout << item4_name << "s " << endl; 
+    cout << "---" << endl;
     cout << "We are only selling " << item1_name << "s" << " today on accout of a recent rogue theft." << endl;
 
     cout << "----------" << endl;
 
-    cout << "We have " << num_item1 << " total in stock." << endl;
+    cout << "We have " << num_item1 << " left in stock." << endl;
     cout << "Each " << item1_name << " is $" << cost_per << " coins" << endl;
 
 
@@ -73,6 +78,7 @@ int main()
 //output
     cout << "----------" << endl;
     cout << "Your total for " << num_wanted << " " << item1_name << "(s) is $" << total_cost << " coins" << endl;
+    
 
 
 

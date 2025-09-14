7cf8604915a1f0f9848c1a12b8a504eb0c7a1981
Mattea <150169210+isleym9447@users.noreply.github.com>
2025-09-14 23:47:27 +0000

    first  drraft

--- M2T2_Isley.cpp
+++ M2T2_Isley.cpp
@@ -24,18 +24,29 @@ int main()
    
 //variables
     const double TAX = 0.08; //tax
-    double cost = 5.99; //meal cost
+    double subtotal = 5.99; //meal cost
+    double tax_amount = subtotal * TAX; //tax amount
+    double total = subtotal + tax_amount; //final total
 
    
 
 
-//input 
+    cout << "=================" << endl;
+    cout << "===Tom's Diner===" << endl;
+    cout << "=================" << endl;
+    cout << endl;
+    cout << "Items in transaction: 1" << endl;
+    cout << "Items:" << endl;
+    cout << "SANDWHICH MEAL (TURKEY) $5.99" << endl;
+    cout << endl;
+    cout << "Subtotal: " << subtotal << endl;
+    cout << "Tax: " << TAX << endl;
+    cout << "Taxed: " << tax_amount << endl;
+    cout << "Total" << total << endl;
 
 
 
-//processing
 
-    cout << "Price: " << cost
 
 
 

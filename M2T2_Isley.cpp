6133493cb9de0e435e8777efb08f5994814abe3c
Mattea <150169210+isleym9447@users.noreply.github.com>
2025-09-15 15:15:50 +0000

    MADE IT PURDY

--- M2T2_Isley.cpp
+++ M2T2_Isley.cpp
@@ -17,6 +17,7 @@ SPECIAL FX:
 //beginning with the magic words
 
 #include <iostream>
+#include <ctime>
 using namespace std;
 
 int main()
@@ -27,6 +28,12 @@ int main()
     double subtotal = 5.99; //meal cost
     double tax_amount = subtotal * TAX; //tax amount
     double total = subtotal + tax_amount; //final total
+    string item = "BACON CHEESE BURGER MEAL";
+    string mod1 = "-MEDIUM WELL";
+    string mod2 = "-EXTRA CHEESE";
+    string side = "FRIES";
+
+
 
    
 
@@ -34,16 +41,30 @@ int main()
     cout << "=================" << endl;
     cout << "===Tom's Diner===" << endl;
     cout << "=================" << endl;
+    cout << "Watson, Night City" << endl;
     cout << endl;
-    cout << "Items in transaction: 1" << endl;
+    cout << "DATE: 9/20/2077     TIME: 22:47" << endl;
+    cout << endl;
+    cout << "CASHIER:  REG-MAIN 1" << endl;
     cout << "Items:" << endl;
-    cout << "SANDWHICH MEAL (TURKEY) $5.99" << endl;
-    cout << endl;
-    cout << "Subtotal: $" << subtotal << endl;
+    cout << "- " << item << "\t" << "$13.99" << endl;
+    cout << "\t" << mod1 << endl;
+    cout << "\t" << mod2 << endl;
+    cout << "- " << side << " \t\t\t $0.50" << endl;
+    cout << "\t" << "-UPCHARGE" << endl;
+
+    cout << "----------------------------" << endl;
+
+
+
+
+
+    cout << "Subtotal: " << "\t$" << subtotal << endl;
     cout << "Tax: " << TAX << endl;
     cout << "Taxed: " << tax_amount << endl;
-    cout << "Total: $" << total << endl;
+    cout << "Total: " << total << endl;
     cout << endl;
+    cout << "Items in transaction: 1" << endl;
 
 
 

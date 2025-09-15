e42fd9e319256765d2745198f23c6a9f2e2d0259
Mattea <150169210+isleym9447@users.noreply.github.com>
2025-09-15 14:36:50 +0000

    menu

---
+++ M2HW1_isley.cpp
@@ -0,0 +1,65 @@
+/*
+CSC 134
+Mattea Isley
+ 9/15/2025
+
+
+INPUT: 
+PROCESSING: 
+OUTPUT: 
+return 0 (no error)
+SPECIAL FX:
+*/
+
+
+
+
+//beginning with the magic words
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+//variables
+
+
+cout << "Gold" << endl;
+cout << endl;
+cout << "Please make a selection" << endl;
+cout << "1: Bank Account Balance" << endl;
+cout << "2: Crate Expectations" << endl;
+cout << "3: Pizza Party" << endl;
+cout << "4: School Cheer" << endl;
+cout << "5: Exit" << endl;
+
+
+   
+
+//input 
+
+
+
+//processing
+
+
+
+
+
+
+//output
+    return 0; //no errors
+
+
+
+
+
+
+
+
+
+
+
+
+}

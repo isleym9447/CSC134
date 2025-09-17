5f9e8f97ca63fb02bbf3d2779cb3bd2d4de48d15
Mattea <150169210+isleym9447@users.noreply.github.com>
2025-09-17 14:22:47 +0000

    M3T1

---
+++ M3/M3T1&2_Isley.cpp
@@ -0,0 +1,51 @@
+/*
+CSC 134
+Mattea Isley
+9/17/2025
+M3T1
+
+INPUT: 
+PROCESSING: 
+OUTPUT: 
+return 0 (no error)
+SPECIAL FX:
+*/
+
+
+
+
+//beginning with the magic words
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+//output
+    return 0; //no errors
+
+
+
+
+
+
+
+
+
+
+
+
+}

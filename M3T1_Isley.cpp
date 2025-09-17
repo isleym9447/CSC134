026765edb01fc68128dd14628d10658c714c269e
Mattea <150169210+isleym9447@users.noreply.github.com>
2025-09-17 14:48:23 +0000

    added ifs

--- M3T1_Isley.cpp
+++ M3T1_Isley.cpp
@@ -49,5 +49,27 @@ int main() {
     cout << "The area of the second rectangle is: " << area2 << endl;
     cout << "-----------------------------------" << endl;
 
+
+    if (area1 > area2) {
+        cout << "The first rectangle is larger." << endl;
+
+    }
+
+    if (area2 > area1) {
+        cout << "The second rectangle is larger." << endl;
+        
+    }
+
+    if (area1 == area2) {
+        cout << "The rectangles are equal!" << endl;
+        
+    }
+
+
+
+
+
+
+
     return 0;
 }

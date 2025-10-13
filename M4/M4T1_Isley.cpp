8a9eae70ef4cbd4b0fbc4618551027567518597f
Mattea <150169210+isleym9447@users.noreply.github.com>
2025-10-13 04:00:43 +0000

    m4t1

---
+++ M4/M4T1_Isley.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+  
+int main()
+{
+
+    // Part 1, just say "hi" five times
+    int count = 1;
+    while (count < 5) {
+        cout << "Hello number " << count << endl;
+    }
+
+    return 0; // done
+}

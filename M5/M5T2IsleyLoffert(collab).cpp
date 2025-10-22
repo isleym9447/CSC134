92c5ea2efd45bed447cc2df3b7227df2d76d459a
Mattea <150169210+isleym9447@users.noreply.github.com>
2025-10-22 15:30:45 +0000

    started :)

--- M5/M5T2IsleyLoffert(collab).cpp
+++ M5/M5T2IsleyLoffert(collab).cpp
@@ -16,18 +16,38 @@ using namespace std;
 
 
 
-void printanswerline();
+void printanswerline(){
+    // prints line of numbers
+
+    cout << "1\n";
+    cout << "2\n";
+    cout << "3\n";
+    cout << "4\n";
+    cout << "5\n";
+    cout << "6\n";
+    cout << "7\n";
+    cout << "8\n";
+    cout << "9\n";
+    cout << "10\n";
+}
 
 
-double square();
+double square(){
+    // squares the number 
+    cout << "1\n";
+}
 
 
 
 
 int main()
 {
+ //calculations and printing 
 
+    cout << "Numbers: " << endl;
+    printanswerline();
 
+    cout << "Squares: " << endl;
 
 
 

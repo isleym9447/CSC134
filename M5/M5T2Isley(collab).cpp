9bf36d0504817d42b1f87bd10c5e8a4311016d69
Mattea <150169210+isleym9447@users.noreply.github.com>
2025-10-22 15:22:04 +0000

    created collab

---
+++ M5/M5T2Isley(collab).cpp
@@ -0,0 +1,42 @@
+/*
+CSC 134
+M5T2
+Lydia Loffert & Mattea Isley
+10/22/2015
+*/
+
+
+
+//beginning with the magic words
+
+#include <iostream>
+using namespace std;
+
+
+
+
+
+void printanswerline();
+
+
+double square();
+
+
+
+
+int main()
+{
+
+
+
+
+
+
+
+
+//output
+    return 0; //no errors
+    
+    
+    
+}

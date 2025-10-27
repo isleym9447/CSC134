090f3e1102701568854716d717655eab3e775d18
Mattea <150169210+isleym9447@users.noreply.github.com>
2025-10-27 02:39:31 -0400

    Create elysuim.cpp

---
+++ Escape from The Underworld/elysuim.cpp
@@ -0,0 +1,14 @@
+#include "Elysium.h"
+#include "elysium_events.h"
+#include "ending.h"
+#include <iostream>
+using namespace std;
+
+bool elysiumChapter(Player& p){
+    cout << "\n🟩 FIELDS OF ELYSIUM\n";
+    cout << "Soft grasses and golden light—yet trials remain.\n";
+
+    if(!elysiumRandomEvent(p)) return false;
+    p.roomsCleared++;
+    return !checkEnding(p);
+}

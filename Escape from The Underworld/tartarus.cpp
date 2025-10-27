bb2df91216cf7c8a54811627f34187861a65972b
Mattea <150169210+isleym9447@users.noreply.github.com>
2025-10-27 02:40:43 -0400

    Create tartarus.cpp

---
+++ Escape from The Underworld/tartarus.cpp
@@ -0,0 +1,14 @@
+#include "Tartarus.h"
+#include "tartarus_events.h"
+#include "ending.h"
+#include <iostream>
+using namespace std;
+
+bool tartarusChapter(Player& p){
+    cout << "\n🟥 TARTARUS DEPTHS\n";
+    cout << "Iron chains clatter; cold wind carries whispers of oaths broken.\n";
+
+    if(!tartarusRandomEvent(p)) return false;
+    p.roomsCleared++;
+    return !checkEnding(p);
+}
